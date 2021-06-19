9b0bd2d4e0a26909307752724fe1f64b16c36689
mitsuhiro0602 <0hornet0@gmail.com>
2021-06-19 17:38:30 +0900

    first-commit

---
+++ server/gql-server.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require('apollo-server');
+require('dotenv').config();
+
+
+
+//types query / mutation / subscription
+const typeDefs = `
+  type Query {
+    totalPosts: Int!
+  }
+
+`
+
+//resolvers
+const resolvers = {
+  Query: {
+    totalPosts: () => 42
+  }
+}
+
+// graphql server
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// port
+apolloServer.listen(process.env.PORT, function() {
+  console.log(`graphql is ready at http://localhost${process.env.PORT}`)
+});
