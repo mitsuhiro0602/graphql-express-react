a8e49251e6988bc9fe8206c40756548d8c37c2e9
mitsuhiro0602 <0hornet0@gmail.com>
2021-06-20 13:50:13 +0900

    graphql react connect and card post implement

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import ApolloClient from 'apollo-boost'
+import { gql } from 'apollo-boost'
 
-function App() {
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
+});
+
+
+
+const App = () => {
+  const [posts, setPosts] = useState([])
+  // console.log(client)
+  client.query({
+    query: gql`
+      {
+        allPosts {
+          id
+          title
+          description
+        }
+      }
+    `
+    
+  }).then(result => setPosts(result.data.allPosts));
+
   return (
-    <div className="App">
-      hello
+    <div className="container">
+      <div className="row p5">
+        {posts.map(p => (
+        <div className="col-md-4" key={p.id}>
+          <div className="card">
+            <div className="card-body">
+              <div className="card-title">
+                <h4>{p.title}</h4>
+              </div>
+                <p className="card-text">{p.description}</p>
+            </div>
+          </div>
+        </div>
+        ))}
+      </div>
     </div>
   );
 }
