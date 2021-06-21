76dca274ec5efea261caa76992f4ad1b944e87f0
mitsuhiro0602 <0hornet0@gmail.com>
2021-06-21 13:47:14 +0900

    logout function implement

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useContext, Fragment } from 'react';
+import { Link, useHistory} from 'react-router-dom';
+import { auth } from '../firebase';
+import { AuthContext } from '../context/authContext';
 
 const Nav = () => {
+  const { state, dispatch } = useContext(AuthContext)
+  let history = useHistory()
+
+  const { user } = state
+  const logout = () => {
+    auth().signOut()
+    dispatch({
+      type: 'LOGGED_IN_USER',
+      payload: null
+    })
+    history.push('/login');
+  }
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
@@ -31,21 +44,27 @@ const Nav = () => {
             <Link className="nav-link" >
             </Link>
           </li>
-          <li className="nav-item active">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </li>
-          <li className="nav-item">
-            <a href="/login" className="nav-item nav-link" to="/logout">
-              Logout
-            </a>
-          </li>
+          {!user && (
+            <Fragment>
+              <li className="nav-item active">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+            </Fragment>
+          )}
+          {user && (
+            <li className="nav-item">
+              <a onClick={logout} href="/login" className="nav-item nav-link">
+                Logout
+              </a>
+            </li>
+          )}
         </ul>
         <div className="ml-auto">
           <form className="form-inline my-2 my-lg-0">
