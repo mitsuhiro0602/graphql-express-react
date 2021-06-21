26f56171f237c72b5c7627328f5a0eca22196b2b
mitsuhiro0602 <0hornet0@gmail.com>
2021-06-21 09:16:44 +0900

    register email form implement

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import ApolloClient from 'apollo-boost'
-import { gql } from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
+import { Switch, Route } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 // import components
-import Home from './pages/Home'
+
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
@@ -14,7 +19,12 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Home />
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
     </ApolloProvider>
   );
 }
---
+++ client/src/pages/auth/Register.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+  return (
+    <div className="container p-5">
+      <h4>Register</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            placeholder="Enter Email"
+            disabled={loading}
+
+          />
+        </div>
+        <button className="btn btn-raised btn-primary" disabled={!email || loading}>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Register;
